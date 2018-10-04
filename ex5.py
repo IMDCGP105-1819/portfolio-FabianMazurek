2aca31b4fa5127a29f1797b5a400950deafcd240
FabianMazurek <fabianmazurek2000@gmail.com>
2018-10-04 16:16:59 +0100

    Final

--- ex5.py
+++ ex5.py
@@ -1,3 +1,4 @@
 bottles = 99
 for bottles in range(99, 0, -1):
     print(str(bottles) + " bottles of beer on the wall, " + str(bottles) + " bottles of beer. " + "Take one down, pass it around, " + str(bottles-1) + "bottles of beer on the wall...")
+print("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall...")
