ec65cde44207380444cb7b468b43e95bf440d604
FabianMazurek <fabianmazurek2000@gmail.com>
2018-10-04 16:06:19 +0100

    got the lyrics to dispaly with changing numbers

--- ex5.py
+++ ex5.py
@@ -1,3 +1,3 @@
 bottles = 99
 for bottles in range(99, 0, -1):
-    print(bottles)
+    print(str(bottles) + " bottles of beer on the wall, " + str(bottles) + " bottles of beer. " + "Take one down, pass it around, " + str(bottles-1) + "bottles of beer on the wall...")
